1ab1bf65a749270dcfee36b5c14dfa6ba945dfb6
Alexander Kochetov <creocoder@gmail.com>
2015-03-31 22:02:31 +0300

    Moved sources to "src" directory, fixed file header

--- src/TranslateableBehavior.php
+++ src/TranslateableBehavior.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-translateable-behavior
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\translateable;
 
 use Yii;
@@ -263,4 +264,4 @@ class TranslateableBehavior extends Behavior
             }
         }
     }
-} 
+}
